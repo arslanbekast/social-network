bedecf17c2449b3a0e6af4b222bec09988e83d15
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-09-23 12:35:42 +0300

    The application is divided into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,15 @@
 import React from 'react';
 import './App.css';
-import logo from './assets/images/logo.png'
-import mainImg from './assets/images/main-image.jpg'
+import {Header} from "./components/Header";
+import {Sidebar} from "./components/Sidebar";
+import {Profile} from "./components/Profile";
 
 function App() {
     return (
         <div className="app-wrapper">
-            <header className='header'>
-                <img src={logo} alt=""/>
-            </header>
-
-            <aside className='sidebar'>
-                <nav className='nav'>
-                    <ul>
-                        <li><a href='#'>Profile</a></li>
-                        <li><a href='#'>Messages</a></li>
-                        <li><a href='#'>News</a></li>
-                        <li><a href='#'>Music</a></li>
-                        <li><a href='#'>Settings</a></li>
-                    </ul>
-                </nav>
-            </aside>
-
-            <main className='content'>
-
-                <div className='main-img_block'>
-                    <img src={mainImg} alt="main image"/>
-                </div>
-
-                <div>
-                    ava + description
-                </div>
-
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        <div>post 1</div>
-                        <div>post 2</div>
-                        <div>post 3</div>
-                    </div>
-                </div>
-
-            </main>
+            <Header/>
+            <Sidebar/>
+            <Profile/>
         </div>
     );
 }
