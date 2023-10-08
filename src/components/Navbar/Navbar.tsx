e7b9623f39a4cd32860fdee6dd595bf57fcead7c
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-10-08 12:37:50 +0300

    Changed Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,11 +6,21 @@ export const Navbar: FC = () => {
         <aside className={s.navbar}>
             <nav className={s.nav}>
                 <ul>
-                    <li className={`${s.item} ${s.active}`}><a href='src/components/Navbar/Navbar#'>Profile</a></li>
-                    <li className={s.item}><a href='src/components/Navbar/Navbar#'>Messages</a></li>
-                    <li className={s.item}><a href='src/components/Navbar/Navbar#'>News</a></li>
-                    <li className={s.item}><a href='src/components/Navbar/Navbar#'>Music</a></li>
-                    <li className={s.item}><a href='src/components/Navbar/Navbar#'>Settings</a></li>
+                    <li className={`${s.item} ${s.active}`}>
+                        <a href='/profile'>Profile</a>
+                    </li>
+                    <li className={s.item}>
+                        <a href='/dialogs'>Dialogs</a>
+                    </li>
+                    <li className={s.item}>
+                        <a href='/news'>News</a>
+                    </li>
+                    <li className={s.item}>
+                        <a href='/music'>Music</a>
+                    </li>
+                    <li className={s.item}>
+                        <a href='/settings'>Settings</a>
+                    </li>
                 </ul>
             </nav>
         </aside>
