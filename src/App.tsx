d53ddfc627c51eac2afcf93409f355e9f220ffa7
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-10-08 12:39:53 +0300

    Used the React Router Dom library for routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,26 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <main className='main-wrapper'>
-                <Dialogs/>
-                {/*<Profile/>*/}
-            </main>
-
-
-        </div>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <main className='main-wrapper'>
+                    <Route path={'/profile'} component={Profile} />
+                    <Route path={'/dialogs'} component={Dialogs} />
+                    <Route path={'/news'} component={News} />
+                    <Route path={'/music'} component={Music} />
+                    <Route path={'/settings'} component={Settings} />
+                </main>
+            </div>
+        </BrowserRouter>
     );
 }
 
