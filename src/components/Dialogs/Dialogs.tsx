14d630505873183775eb6a86777ebcbb9b38a57e
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-10-21 16:31:12 +0300

    Refactoring Dialogs component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,7 +3,7 @@ import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
 type DialogItemPropsType = {
-    id: string
+    id: number
     name: string
 }
 
@@ -25,22 +25,35 @@ const Message: FC<MessagePropsType> = ({message}) => {
 }
 
 export const Dialogs: FC = () => {
+
+    const dialogsData = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Viktor'},
+        {id: 6, name: 'Valera'}
+    ]
+
+    const messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your it'},
+        {id: 3, message: 'Yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'Yo'}
+    ]
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem id='1' name='Dimych' />
-                <DialogItem id='2' name='Andrey' />
-                <DialogItem id='3' name='Sveta' />
-                <DialogItem id='4' name='Sasha' />
-                <DialogItem id='5' name='Vuktor' />
-                <DialogItem id='6' name='Valera' />
+                {
+                    dialogsData.map(d => <DialogItem key={d.id} id={d.id} name={d.name} />)
+                }
             </div>
             <div className={s.messages}>
-                <Message message='Hi' />
-                <Message message='How is your it' />
-                <Message message='Yo' />
-                <Message message='Yo' />
-                <Message message='Yo' />
+                {
+                    messagesData.map(m => <Message key={m.id} message={m.message} />)
+                }
             </div>
         </div>
     );
