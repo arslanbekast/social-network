9cbf925a0f2779b32bd0b56a5f6c45b5e30698d3
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-10-21 17:06:52 +0300

    Refactoring MyPosts component

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,14 @@ import s from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 
 export const MyPosts: FC = () => {
+
+    const postsData = [
+        {id: 1, message: "Hi, how are you?", likesCount: 0},
+        {id: 2, message: "It's my first post", likesCount: 25},
+        {id: 2, message: "Blabla", likesCount: 13},
+        {id: 2, message: "Dadad", likesCount: 22},
+    ]
+
     return (
         <div className={s.postsBlock}>
             <h2>My posts</h2>
@@ -16,8 +24,9 @@ export const MyPosts: FC = () => {
 
             </div>
             <div className={s.posts}>
-                <Post message={'Hi, how are you?'} likesCount={0}/>
-                <Post message="It's my first post" likesCount={25}/>
+                {
+                    postsData.map(p => <Post key={p.id} message={p.message} likesCount={p.likesCount}/>)
+                }
             </div>
         </div>
     );
