e01f27c43db53229ce5a0cfb2baf3b93221e16b5
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-12-03 18:15:12 +0300

    Added UsersContainer to Route in App

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import {Settings} from "./components/Settings/Settings";
 import {ActionsType, StateType} from "./redux/store";
 import {StoreType} from "./redux/redux-store";
 import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
+import {Users} from "./components/Users/Users";
+import UsersContainer from "./components/Users/UsersContainer";
 
 type AppPropsType = {
     // store: StoreType
@@ -26,6 +28,7 @@ function App() {
                     <Route path={'/profile'} render={ () => <Profile /> } />
 
                     <Route path={'/dialogs'} render={ () => <DialogsContainer /> } />
+                    <Route path={'/users'} render={ () => <UsersContainer /> } />
                     <Route path={'/news'} component={News} />
                     <Route path={'/music'} component={Music} />
                     <Route path={'/settings'} component={Settings} />
