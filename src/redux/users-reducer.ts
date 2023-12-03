7ba21b656fef8b34d5584f27c083caa048f9d0e2
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-12-03 18:13:30 +0300

    Created users reducer

---
+++ src/redux/users-reducer.ts
@@ -0,0 +1,62 @@
+import {ActionsType, ProfilePageType} from "./store";
+import {v1} from "uuid";
+
+const FOLLOW = 'FOLLOW' as const
+const UNFOLLOW = 'UNFOLLOW' as const
+const SET_USERS = 'SET_USERS' as const
+
+type LocationType = {
+    city: string
+    country: string
+}
+export type UsersType = {
+    id: number
+    photoUrl: string
+    followed: boolean
+    fullName: string
+    status: string
+    location: LocationType
+}
+type UsersStateType = {
+    users: UsersType[]
+}
+
+const initialState = {
+        users: [
+            // {id: 1, photoUrl: 'https://cybersecurity-excellence-awards.com/wp-content/uploads/2021/01/676420.jpg', followed: false, fullName: "Dmitry", status: 'I am boss', location: {city: 'Minsk', country: 'Belarus'}},
+            // {id: 2, photoUrl: 'https://cybersecurity-excellence-awards.com/wp-content/uploads/2021/01/676420.jpg', followed: true, fullName: "Sasha", status: 'I am boss too', location: {city: 'Moscow', country: 'Russia'}},
+            // {id: 3, photoUrl: 'https://cybersecurity-excellence-awards.com/wp-content/uploads/2021/01/676420.jpg', followed: false, fullName: "Andrew", status: 'I am boss too', location: {city: 'Kiev', country: 'Ukraine'}}
+        ]
+    }
+
+export const usersReducer = (state: UsersStateType = initialState, action: UsersActionsType): UsersStateType => {
+
+    switch (action.type) {
+        case FOLLOW:
+            return {
+                ...state,
+                users: state.users.map(u => u.id === action.userId ? {...u, followed: true} : u)
+            }
+        case UNFOLLOW:
+            return {
+                ...state,
+                users: state.users.map(u => u.id === action.userId ? {...u, followed: false} : u)
+            }
+        case SET_USERS:
+            return {...state, users: [...state.users, ...action.users]}
+        default:
+            return state
+    }
+
+}
+
+export type UsersActionsType = FollowActionType | UnFollowActionType | SetUsersActionType
+
+type FollowActionType = ReturnType<typeof followAC>
+export const followAC = (userId: number) => ( {type: FOLLOW, userId} )
+
+type UnFollowActionType = ReturnType<typeof unFollowAC>
+export const unFollowAC = (userId: number) => ( {type: UNFOLLOW, userId} )
+
+type SetUsersActionType = ReturnType<typeof setUsersAC>
+export const setUsersAC = (users: UsersType[]) => ( {type: SET_USERS, users} )
