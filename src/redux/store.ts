7740ecf639d108f4c0a85319af5e0ccaf9ea6607
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-12-03 18:48:16 +0300

    Refactoring store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,3 @@
-import {message} from "antd";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {sidebarReducer} from "./sidebar-reducer";
@@ -54,55 +53,55 @@ export type ActionsType = AddPostActionType
     | UpdateNewMessageTextActionType
     | SendMessageActionType
 
-export const store = {
-    _state: {
-        profilePage: {
-            posts: [
-                {id: 1, message: "Hi, how are you?", likesCount: 0},
-                {id: 2, message: "It's my first post", likesCount: 25},
-                {id: 2, message: "Blabla", likesCount: 13},
-                {id: 2, message: "Dadad", likesCount: 22},
-            ],
-            newPostText: 'it-kamasutra.com'
-        },
-        dialogsPage: {
-            dialogs: [
-                {id: 1, name: 'Dimych'},
-                {id: 2, name: 'Andrey'},
-                {id: 3, name: 'Sveta'},
-                {id: 4, name: 'Sasha'},
-                {id: 5, name: 'Viktor'},
-                {id: 6, name: 'Valera'}
-            ],
-            messages: [
-                {id: 1, message: 'Hi'},
-                {id: 2, message: 'How is your it'},
-                {id: 3, message: 'Yo'},
-                {id: 4, message: 'Yo'},
-                {id: 5, message: 'Yo'}
-            ],
-            newMessageText: ''
-        },
-        sidebar: {}
-    },
-    _callSubscriber(state: StateType) {
-        console.log('State changed')
-    },
-    getState() {
-        return this._state
-    },
-    subscribe(observer: (state: StateType) => void) {
-        this._callSubscriber = observer;
-    },
-    dispatch(action: ActionsType) { // { type: 'ADD-POST' }
-
-        this._state.profilePage = profileReducer(this._state.profilePage, action)
-        this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action)
-        this._state.sidebar = sidebarReducer(this._state.sidebar, action)
-
-        this._callSubscriber(this.getState());
-    }
-}
+// export const store = {
+//     _state: {
+//         profilePage: {
+//             posts: [
+//                 {id: 1, message: "Hi, how are you?", likesCount: 0},
+//                 {id: 2, message: "It's my first post", likesCount: 25},
+//                 {id: 2, message: "Blabla", likesCount: 13},
+//                 {id: 2, message: "Dadad", likesCount: 22},
+//             ],
+//             newPostText: 'it-kamasutra.com'
+//         },
+//         dialogsPage: {
+//             dialogs: [
+//                 {id: 1, name: 'Dimych'},
+//                 {id: 2, name: 'Andrey'},
+//                 {id: 3, name: 'Sveta'},
+//                 {id: 4, name: 'Sasha'},
+//                 {id: 5, name: 'Viktor'},
+//                 {id: 6, name: 'Valera'}
+//             ],
+//             messages: [
+//                 {id: 1, message: 'Hi'},
+//                 {id: 2, message: 'How is your it'},
+//                 {id: 3, message: 'Yo'},
+//                 {id: 4, message: 'Yo'},
+//                 {id: 5, message: 'Yo'}
+//             ],
+//             newMessageText: ''
+//         },
+//         sidebar: {}
+//     },
+//     _callSubscriber(state: StateType) {
+//         console.log('State changed')
+//     },
+//     getState() {
+//         return this._state
+//     },
+//     subscribe(observer: (state: StateType) => void) {
+//         this._callSubscriber = observer;
+//     },
+//     dispatch(action: ActionsType) { // { type: 'ADD-POST' }
+//
+//         this._state.profilePage = profileReducer(this._state.profilePage, action)
+//         this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action)
+//         this._state.sidebar = sidebarReducer(this._state.sidebar, action)
+//
+//         this._callSubscriber(this.getState());
+//     }
+// }
 
 
 
