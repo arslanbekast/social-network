3cdd3f5420297ed47665be2ff37d40eedb8e07d6
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-12-04 21:39:17 +0300

    The Users functional component has been converted to a class component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,51 +1,60 @@
 import React, {FC} from 'react';
-import {UsersType} from "../../redux/users-reducer";
+import {UserType} from "../../redux/users-reducer";
 import s from './Users.module.css'
 import axios from "axios";
 import noPhoto from '../../assets/images/noPhoto.jpg'
 
 type UsersPropsType = {
-    users: UsersType[]
+    users: UserType[]
     follow: (userId: number) => void
     unfollow: (userId: number) => void
-    setUsers: (users: UsersType[]) => void
+    setUsers: (users: UserType[]) => void
 }
 
-export const Users: FC<UsersPropsType> = (props) => {
+export class Users extends React.Component<UsersPropsType, any>{
 
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => props.setUsers(response.data.items))
+    constructor(props: UsersPropsType) {
+        super(props);
+        this.getUsers()
     }
 
-    return (
-        <div className={s.usersContent}>
-            {
-                props.users.map(u => {
-                    return (
-                        <div key={u.id} className={s.usersBox}>
-                            <div className={s.imgBox}>
-                                <div>
-                                    <img src={u.photos.small !== null ? u.photos.small : noPhoto} className={s.userPhoto}/>
+    getUsers = () => {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => this.props.setUsers(response.data.items))
+    }
+
+    render() {
+        return (
+            <div className={s.usersContent}>
+                {/*<button onClick={this.getUsers}>Get Users</button>*/}
+                {
+                    this.props.users.map((u: UserType) => {
+                        return (
+                            <div key={u.id} className={s.usersBox}>
+                                <div className={s.imgBox}>
+                                    <div>
+                                        <img src={u.photos.small !== null ? u.photos.small : noPhoto}
+                                             className={s.userPhoto}/>
+                                    </div>
+                                    <div>
+                                        {u.followed
+                                            ? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
+                                            : <button onClick={() => this.props.follow(u.id)}>Follow</button>}
+                                    </div>
                                 </div>
                                 <div>
-                                    {u.followed
-                                        ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
-                                        : <button onClick={() => props.follow(u.id)}>Follow</button>}
+                                    <div>{u.name}</div>
+                                    <div>{u.status}</div>
+                                </div>
+                                <div>
+                                    <div>{'u.location.country'}</div>
+                                    <div>{'u.location.city'}</div>
                                 </div>
                             </div>
-                            <div>
-                                <div>{u.name}</div>
-                                <div>{u.status}</div>
-                            </div>
-                            <div>
-                                <div>{'u.location.country'}</div>
-                                <div>{'u.location.city'}</div>
-                            </div>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    );
+                        )
+                    })
+                }
+            </div>
+        );
+    }
 };
