d92d2ac1fdb0eaf33abc1dff246d67889eda563e
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-12-16 12:06:41 +0300

    Created Preloader component

---
+++ src/components/Preloader/Preloader.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Preloader.css'
+
+export const Preloader = () => {
+    return (
+        <>
+            <div className="cssload-dots-box">
+                <div className="cssload-dots">
+                    <div className="cssload-dot"></div>
+                    <div className="cssload-dot"></div>
+                    <div className="cssload-dot"></div>
+                    <div className="cssload-dot"></div>
+                    <div className="cssload-dot"></div>
+                </div>
+            </div>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                    <filter id="goo">
+                        <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="12"></feGaussianBlur>
+                        <feColorMatrix in="blur" mode="matrix"
+                                       values="1 0 0 0 0	0 1 0 0 0	0 0 1 0 0	0 0 0 18 -7"
+                                       result="goo"></feColorMatrix>
+                        {/*// <!--<feBlend in2="goo" in="SourceGraphic" result="mix" ></feBlend>-->*/}
+                    </filter>
+                </defs>
+            </svg>
+        </>
+    );
+};
