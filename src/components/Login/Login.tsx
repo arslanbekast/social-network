5a0f9e6ab40035937e03d6c7b4a9f39465473817
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-12-30 19:44:16 +0300

    The implementation of authorization on the site using the redux-form library has begun

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
 
 export const Login = () => {
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
     return (
         <div>
             <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
         </div>
     );
 };
+
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
+
+const LoginForm: FC<InjectedFormProps<FormDataType>> = (props) => {
+
+    return (
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <Field name="login" placeholder="Login" component="input"/>
+                </div>
+                <div>
+                    <Field name="password" placeholder="Password" component="input"/>
+                </div>
+                <div>
+                    <Field type="checkbox" name="rememberMe" id="rememberMe" component="input"/>
+                    <label htmlFor="rememberMe">Remember me</label>
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
+    );
+};
+
+const LoginReduxForm = reduxForm<FormDataType>({
+    form: 'login'
+})(LoginForm)
