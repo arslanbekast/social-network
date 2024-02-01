8792f091ac99f4cf3b1672594a76ed8fefebc823
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-02-01 14:46:49 +0300

    Refactoring api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import {ProfileDataFormType} from "../components/Profile/ProfileInfo/ProfileDataForm/ProfileDataForm";
+import {PhotosType, ProfileType} from "../redux/profile-reducer";
+import {UserType} from "../redux/users-reducer";
 
 
 const instance = axios.create({
@@ -9,15 +11,15 @@ const instance = axios.create({
 
 export const usersAPI = {
     getUsers(currentPage: number = 1, pageSize: number = 10) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+        return instance.get<GetUsersResponse>(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => response.data)
     },
     follow(userId: number) {
-        return instance.post(`follow/${userId}`)
+        return instance.post<BaseResponseType>(`follow/${userId}`)
             .then(response => response.data)
     },
     unFollow(userId: number) {
-        return instance.delete(`follow/${userId}`)
+        return instance.delete<BaseResponseType>(`follow/${userId}`)
             .then(response => response.data)
     },
     getProfile(userId: number) {
@@ -28,47 +30,72 @@ export const usersAPI = {
 
 export const profileAPI = {
     getProfile(userId: number) {
-        return instance.get(`profile/${userId}`)
+        return instance.get<ProfileType>(`profile/${userId}`)
             .then(response => response.data)
     },
     getStatus(userId: number) {
-        return instance.get(`profile/status/${userId}`)
+        return instance.get<string>(`profile/status/${userId}`)
             .then(response => response.data)
     },
     updateStatus(status: string) {
-        return instance.put(`profile/status`, {status})
+        return instance.put<BaseResponseType>(`profile/status`, {status})
             .then(response => response.data)
     },
     savePhoto(file: File) {
         const formData = new FormData()
         formData.append("image", file)
-        return instance.put(`profile/photo`, formData, {headers: {'Content-Type': 'multipart/form-data'}})
+        return instance.put<BaseResponseType<{photos: PhotosType}>>(`profile/photo`, formData, {headers: {'Content-Type': 'multipart/form-data'}})
             .then(response => response.data)
     },
     saveProfile(profile: ProfileDataFormType) {
-        return instance.put(`profile`, profile)
+        return instance.put<BaseResponseType>(`profile`, profile)
             .then(response => response.data)
     }
 }
 
 export const authAPI = {
     me() {
-        return instance.get(`auth/me`)
+        return instance.get<BaseResponseType<GetAuthMeResponseData>>(`auth/me`)
             .then(response => response.data)
     },
     login(email: string, password: string, rememberMe: boolean = false, captcha: string | null = null) {
-        return instance.post(`auth/login`, {email, password, rememberMe, captcha})
+        return instance.post<BaseResponseType<{userId: number}>>(`auth/login`, {email, password, rememberMe, captcha})
             .then(response => response.data)
     },
     logout() {
-        return instance.delete(`auth/login`)
+        return instance.delete<BaseResponseType>(`auth/login`)
             .then(response => response.data)
     }
 }
 
 export const securityAPI = {
     getCaptchaUrl() {
-        return instance.get(`security/get-captcha-url`)
+        return instance.get<{url: string}>(`security/get-captcha-url`)
             .then(response => response.data)
     }
+}
+
+
+type FieldErrorType = {
+    error: string;
+    field: string;
+};
+
+export type BaseResponseType<D = {}> = {
+    resultCode: number;
+    messages: Array<string>;
+    data: D;
+    fieldsErrors: FieldErrorType[];
+};
+
+type GetUsersResponse = {
+    items: UserType[]
+    totalCount: number
+    error: string
+}
+
+type GetAuthMeResponseData = {
+    id: number
+    email: string
+    login: string
 }
