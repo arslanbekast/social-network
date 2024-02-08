87cbfab53802c7d01d5fdca5e971f09de6922bd2
Arslanbek Astemirov <s-o-f-t@mail.ru>
2024-02-08 20:37:00 +0300

    Stylized Post component

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -9,12 +9,33 @@ type PostPropsType = {
 
 export const Post: FC<PostPropsType> = ({message, likesCount}) => {
     return (
-        <div className={s.item}>
-            <img src={noPhoto} alt=""/>
-            {message}
-            <div>
-                <button>Like </button> {likesCount}
+        <div className={s.postWrapper}>
+            <div className={s.post}>
+                <div className={s.postAuthor}>
+                    <div className={s.authorPhoto}>
+                        <img src={noPhoto} alt=""/>
+                    </div>
+
+                    <div className={s.authorNameWrapper}>
+                        <span className={s.authorName}>Victor Exrixon</span>
+                        <span className={s.postTime}>22 min ago</span>
+                    </div>
+
+                </div>
+
+                <div className={s.postText}>
+                    {message}
+                    <span className={s.seeMore}>
+                        <a href="#">See more</a>
+                    </span>
+                </div>
+
+                <div className={s.postBottom}>
+                    <button className={s.likeBtn}><i className="fa-solid fa-thumbs-up"></i></button>
+                    <span className={s.likesCount}>{likesCount}</span>
+                </div>
             </div>
         </div>
+
     );
 };
